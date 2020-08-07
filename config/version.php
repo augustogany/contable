80ca5990c9c7c2ac940079478d9fed128b2dbfcd
Augusto carvalho <augustogany@gmail.com>
2020-08-07 09:21:25 -0400

    first commit

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Argon',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '0',
+
+    'patch'     =>  '19',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '27-July-2020',
+
+    'time'      =>  '19:00',
+
+    'zone'      =>  'GMT +3',
+
+];
---
+++ resources/lang/id-ID/taxes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'rate'                  => 'Kurs',
+    'rate_percent'          => 'Kurs (%)',
+    'normal'                => 'Normal',
+    'inclusive'             => 'Inklusif',
+    'compound'              => 'Campuran',
+    'fixed'                 => 'Diperbaiki',
+    'withholding'           => 'Withholding',
+];
